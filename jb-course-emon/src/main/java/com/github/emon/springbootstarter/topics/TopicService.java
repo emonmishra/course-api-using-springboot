582817516b6ad9fae2003670d814fdbb2af8f3b8
Emon Mishra <mishraemon@gmail.com>
2017-01-03 00:17:19 +0530

    Moved list creation logic in controller to Service

---
+++ jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicService.java
@@ -0,0 +1,20 @@
+package com.github.emon.springbootstarter.topics;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
+			new Topic("angular", "Angular 2", "Angular 2 Course Quickstart"),
+			new Topic("django", " django frawemwork", "Django Frame Work in Detail"));
+
+	public List<Topic> getAllTopics() {
+
+		return topics;
+	}
+
+}
--- jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicsController.java
+++ jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicsController.java
@@ -3,19 +3,20 @@ package com.github.emon.springbootstarter.topics;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicsController {
 
+	@Autowired
+	private TopicService topicService;
+
 	@RequestMapping("/topics")
 	public List<Topic> getTopics() {
 
-		return Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
-				new Topic("angular", "Angular 2", "Angular 2 Course Quickstart"),
-				new Topic("django", " django frawemwork", "Django Frame Work in Detail"));
-
+		return topicService.getAllTopics();
 	}
 
 }
