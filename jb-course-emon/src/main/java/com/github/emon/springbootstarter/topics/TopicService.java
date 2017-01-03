4ad59a93fee6bdf5afa4fe5bc5cfc998b982da4f
Emon Mishra <mishraemon@gmail.com>
2017-01-03 20:32:00 +0530

    added put and delete methods

--- jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicService.java
+++ jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicService.java
@@ -1,5 +1,6 @@
 package com.github.emon.springbootstarter.topics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,18 +9,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("java", "Core Java", "Core Java Course"),
 			new Topic("angular", "Angular 2", "Angular 2 Course Quickstart"),
-			new Topic("django", " django frawemwork", "Django Frame Work in Detail"));
+			new Topic("django", " django frawemwork", "Django Frame Work in Detail")));
 
 	public List<Topic> getAllTopics() {
 
 		return topics;
 	}
-	
-	public Topic getTopic(String id){
-		
+
+	public Topic getTopic(String id) {
+
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public boolean addTopic(Topic topic) {
+
+		return topics.add(topic);
+	}
+
+	public Topic updateTopic(Topic topic, String id) {
+
+		for (int i = 0; i < topics.size(); i++) {
+
+			if (topics.get(i).getId().equals(id)) {
+				return topics.set(i, topic);
+			}
+		}
+
+		return null;
+	}
+
+	public boolean deleteTopic(String id) {
+
+		return topics.removeIf(t -> t.getId().equals(id));
 	}
 
 }
--- jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicsController.java
+++ jb-course-emon/src/main/java/com/github/emon/springbootstarter/topics/TopicsController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,11 +20,26 @@ public class TopicsController {
 
 		return topicService.getAllTopics();
 	}
-	
-	@RequestMapping("/topics/{topic}")
-	public Topic getTopic(@PathVariable("topic") String id){
-		
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable("id") String id) {
+
 		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public boolean addTopic(@RequestBody Topic topic) {
+		return topicService.addTopic(topic);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public Topic updateTopic(@RequestBody Topic topic, @PathVariable("id") String id) {
+		return topicService.updateTopic(topic, id);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public boolean deleteTopic(@PathVariable("id") String id) {
+		return topicService.deleteTopic(id);
 	}
 
 }
